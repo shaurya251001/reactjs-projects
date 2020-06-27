c978ece201fd31874ed025688cb323ff83f72f25
shaurya251001 <shauryamarwah02@gmail.com>
2020-06-27 10:55:03 +0530

    adding new todo

--- src/App.js
+++ src/App.js
@@ -39,12 +39,22 @@ class App extends Component {
       todos: [...this.state.todos.filter((todo) => todo.id !== id)],
     });
   };
+
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo} />
           <Todos
             todos={this.state.todos}
             markComplete={this.markComplete}
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 
 export class AddTodo extends Component {
+  state = {
+    title: "",
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({ title: "" });
+  };
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
   render() {
     return (
-      <form style={{ display: "flex" }}>
+      <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
         <input
           type="text"
           name="title"
           placeholder="Add Todo ..."
           style={{ flex: "10", padding: "5px" }}
+          onChange={this.onChange}
         />
         <input
           type="submit"
