a7a4621539d6e2c94ac01c028bb8cd88b6c27ea6
shaurya251001 <shauryamarwah02@gmail.com>
2020-06-27 14:22:07 +0530

    added about route

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a TodoList app , it is a part of the React crash course</p>
+    </React.Fragment>
+  );
+}
