8f5b0a6873b84d9825f7ef5ebeb3f87d61e7c2b8
shaurya251001 <shauryamarwah02@gmail.com>
2020-06-27 07:07:07 +0530

    toggle completed todos

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -3,16 +3,36 @@ import Todos from "./Todos";
 import PropTypes from "prop-types";
 
 export class Todoitem extends Component {
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
+
   render() {
+    const { id, title } = this.props.todo;
     return (
-      <div>
-        <p>{this.props.todo.title}</p>
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+        </p>
       </div>
     );
   }
 }
 Todoitem.propTypes = {
   todos: PropTypes.array.isRequired,
+};
+
+const ItemStyle = {
+  backgroundColor: "#f4f4f4",
 };
 
 export default Todoitem;
