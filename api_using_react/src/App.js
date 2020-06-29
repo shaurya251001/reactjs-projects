077416868116b9f18e5b045b24f762ebd615a6b2
shaurya251001 <shauryamarwah02@gmail.com>
2020-06-29 21:59:47 +0530

    components added

--- api_using_react/src/App.js
+++ api_using_react/src/App.js
@@ -1,27 +1,48 @@
 import React from "react";
 import "./App.css";
 import Input from "./components/Input";
+import Card from "./components/Card";
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      input: "",
+      data: [],
+    };
   }
-  // componentDidMount() {
-  //   const APP_ID = "decaff6f";
-  //   const APP_KEY = "23a4322e4bc2db26f6ba1be425ad8d75";
-  //   fetch(
-  //     `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
-  //   )
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => data.hits)
-  //     .catch((e) => console.log(e));
-  // }
+
+  componentDidMount() {
+    const APP_ID = "decaff6f";
+    const APP_KEY = "23a4322e4bc2db26f6ba1be425ad8d75";
+    fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const newA = data.hits.map(function (hit) {
+          return (
+            <Card
+              url={hit.recipe.url}
+              img={hit.recipe.image}
+              ing={hit.recipe.ingredients}
+              title={hit.recipe.label}
+            />
+          );
+        });
+        this.setState({
+          data: newA,
+        });
+      });
+  }
+
   render() {
     return (
       <div className="row">
         <Input />
+        {this.state.data}
       </div>
     );
   }
---
+++ api_using_react/src/components/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className="col s12 m3">
+      <div className="card">
+        <div className="card-image">
+          <img src={props.img} />
+          <span className="card-title">{props.title}</span>
+        </div>
+        <div className="card-Namecontent">
+          <p></p>
+        </div>
+        <div className="card-action center-align">
+          <a href={props.url} className="btn waves-effect yellow">
+            Recipe
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
