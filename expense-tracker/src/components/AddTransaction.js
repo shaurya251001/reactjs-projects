4a9be6bc05c9d2a4cc9ecf9e66855bd367373b82
shaurya251001 <shauryamarwah02@gmail.com>
2020-06-30 14:54:44 +0530

    state using react hooks added

---
+++ expense-tracker/src/components/AddTransaction.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export default function AddTransaction() {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form id="form">
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            id="text"
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            type="number"
+            id="amount"
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+}
