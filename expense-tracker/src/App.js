8a1bac8bba5a473bd96356f8f99efd3d730f5b99
shaurya251001 <shauryamarwah02@gmail.com>
2020-06-30 14:21:25 +0530

    TransactionList component added

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
