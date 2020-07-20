c1a14ea60bde8096f835db263db65346332da8ca
shaurya251001 <shauryamarwah02@gmail.com>
2020-07-20 06:01:24 +0530

    build it adding state to visibility toggle

--- build-it-visible-app/src/app.js
+++ build-it-visible-app/src/app.js
@@ -1,19 +1,28 @@
-console.log("Ho ");
-let visibility = false;
-const handleClick = () => {
-  visibility = !visibility;
-  render();
-};
-const render = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={handleClick}>
-        {visibility ? "Hide Details" : "Show Details"}
-      </button>
-      {visibility && <p>{"Some text to see"}</p>}
-    </div>
-  );
-  ReactDOM.render(template, document.querySelector(".app"));
-};
-render();
+class VisibiltyToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false,
+    };
+    this.handleToggleVisibilty = this.handleToggleVisibilty.bind(this);
+  }
+  handleToggleVisibilty() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility,
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibilty}>
+          {this.state.visibility ? "Hide Details" : "Show Details"}
+        </button>
+        {this.state.visibility && <p>Some text to see</p>}
+      </div>
+    );
+  }
+}
+ReactDOM.render(<VisibiltyToggle />, document.querySelector(".app"));
