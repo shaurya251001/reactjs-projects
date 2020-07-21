dfe896d521037d984696dfccb1cfc02a8c06b9ea
shaurya251001 <shauryamarwah02@gmail.com>
2020-07-21 13:55:42 +0530

    setting up babel with webpack

--- indecision_app/src/app.js
+++ indecision_app/src/app.js
@@ -1,18 +1,9 @@
-// import subtract, { square, add } from "./utils"; //with default exports you can ref it with any name you want
-// import isSenior, { isAdult, canDrink } from "./person";
-// console.log(square(2));
-// console.log(add(2, 564));
-
-// console.log(isAdult(12));
-// console.log(canDrink(71));
-// console.log(subtract(3252, 9238));
-// console.log(isSenior(66));
-// import validator from "validator";
-
-// console.log(validator.isEmail("test@gm.com"));
 import React from "react";
 import ReactDOM from "react-dom";
 
-const template = React.createElement("p", {}, "xyz");
+const template = <p>Xyz</p>;
 
 ReactDOM.render(template, document.querySelector(".app"));
+//loader
+
+//babel-core runs babel through webpacks
