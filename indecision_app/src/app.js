54bfffd69fec78408d0de5d56bfae5d84505bd7c
shaurya251001 <shauryamarwah02@gmail.com>
2020-07-29 10:13:26 +0530

    es6 class properties

--- indecision_app/src/app.js
+++ indecision_app/src/app.js
@@ -3,3 +3,25 @@ import ReactDOM from "react-dom";
 import Indecision from "./components/Indecision";
 
 ReactDOM.render(<Indecision />, document.querySelector(".app"));
+// class OldSyntax {
+//   constructor() {
+//     this.name = "xyz";
+//     this.getGreeting = this.getGreeting.bind(this);
+//   }
+//   getGreeting() {
+//     return `Hi ${this.name}`;
+//   }
+// }
+// const oldSyntax = new OldSyntax();
+// const getGreeting = oldSyntax.getGreeting;
+// console.log(getGreeting());
+
+// class NewSyntax {
+//   name = "xyz";
+//   getGreeting = () => {
+//     return `Hi ${this.name}`;
+//   };
+// }
+// const newSyntax = new NewSyntax();
+// const newGetGreeting = newSyntax.getGreeting;
+// console.log(newGetGreeting());
--- indecision_app/src/components/AddOption.js
+++ indecision_app/src/components/AddOption.js
@@ -1,14 +1,10 @@
 import React from "react";
 
 export default class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined,
-    };
-  }
-  handleAddOption(e) {
+  state = {
+    error: undefined,
+  };
+  handleAddOption = (e) => {
     e.preventDefault();
     const option = e.target.elements.addoptions.value.trim();
 
@@ -16,7 +12,7 @@ export default class AddOption extends React.Component {
 
     this.setState(() => ({ error }));
     if (!error) e.target.elements.addoptions.value = "";
-  }
+  };
   render() {
     return (
       <div>
