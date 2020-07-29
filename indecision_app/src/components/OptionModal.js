d40106961593c1e063cdc1d27f6406fa73cb0c21
shaurya251001 <shauryamarwah02@gmail.com>
2020-07-29 11:00:46 +0530

    setting up react modal --some-changes

--- indecision_app/src/components/OptionModal.js
+++ indecision_app/src/components/OptionModal.js
@@ -3,7 +3,11 @@ import Modal from "react-modal";
 
 const OptionModal = (props) => {
   return (
-    <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
+    <Modal
+      isOpen={!!props.selectedOption}
+      contentLabel="Selected Option"
+      onRequestClose={props.click}
+    >
       <h1>Selected Option</h1>
       <p>{props.selectedOption}</p>
       <button onClick={props.click}>Okay</button>
