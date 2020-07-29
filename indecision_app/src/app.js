2b75b2c94f1bccd503b1cb5c92a6ecde685dadcd
shaurya251001 <shauryamarwah02@gmail.com>
2020-07-29 10:27:13 +0530

    Passing children to a component

--- indecision_app/src/app.js
+++ indecision_app/src/app.js
@@ -2,6 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Indecision from "./components/Indecision";
 
+// const Layout = (props) => {
+//   return (
+//     <div>
+//       <p>header</p>
+//       {props.children}
+//       <p>footer</p>
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(
+//   <Layout>
+//     <div>
+//       <h1>Page Title</h1>
+//       <p>This is a page</p>
+//     </div>
+//   </Layout>,
+//   document.querySelector(".app")
+// );
 ReactDOM.render(<Indecision />, document.querySelector(".app"));
 // class OldSyntax {
 //   constructor() {
