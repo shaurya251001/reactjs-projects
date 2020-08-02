afdc3c44ca661f832b7a158e82c6637d7f122af0
shaurya251001 <shauryamarwah02@gmail.com>
2020-08-02 17:17:39 +0530

    dispatching actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,37 @@
 import { createStore } from "redux"; //you have to createstore to start working with redux
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1,
+      };
+    case "RESET":
+      return {
+        count: 0,
+      };
+    default:
+      return state;
+  }
 });
 
 console.log(store.getState()); //gets the current state
+//Action-object that gets sent to the store
+store.dispatch({
+  type: "INCREMENT",
+});
+store.dispatch({
+  type: "DECREMENT",
+});
+store.dispatch({
+  type: "DECREMENT",
+});
+console.log(store.getState());
+store.dispatch({
+  type: "RESET",
+});
+console.log(store.getState());
