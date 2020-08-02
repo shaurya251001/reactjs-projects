bd9d01f27e7971ea962003d82b49c1d4c9e8fe08
shaurya251001 <shauryamarwah02@gmail.com>
2020-08-03 05:07:37 +0530

    array destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -16,14 +16,30 @@
 //   console.log(`Its ${temperature} in ${city}`);
 // //just in case you did'nt want to use the names of the objects methods'.
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {
-    name: "Penguin",
-  },
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {
+//     name: "Penguin",
+//   },
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
+// const { name: publisherName = "Self-Published" } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//Array destructuring
+
+const address = [
+  "1200 S Juniper Street",
+  "Philadelphia",
+  "pennyslyvania",
+  "20000",
+];
+const [, city, state = "New York"] = address;
+console.log(`You are in ${city} ${state}`);
+
+const item = ["Coffee(hot)", "$2.00", "$2.50", "$2.75"];
+
+const [ind_item, , med_price] = item;
+console.log(`A medium ${ind_item} costs ${med_price}`);
