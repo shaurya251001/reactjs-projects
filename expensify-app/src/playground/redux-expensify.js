c443a52634ae887f67fce375c2f52de0cfb6916f
shaurya251001 <shauryamarwah02@gmail.com>
2020-08-03 13:39:44 +0530

    spreading objects

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -28,7 +28,22 @@ const removeExpense = ({ id } = {}) => {
   };
 };
 //EDIT_EXPENSE
+
+const editExpense = (id, updates) => {
+  return {
+    type: "EDIT_EXPENSE",
+    id,
+    updates,
+  };
+};
 //SET_TEXT_FILTER
+
+const setTextFilter = (text = "") => {
+  return {
+    type: "SET_TEXT_FILTER",
+    text,
+  };
+};
 //SORT_BY_DATE
 //SORT_BY_AMOUNT
 //SET_START_DATE
@@ -46,6 +61,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
         }
       });
       return updateState;
+    case "EDIT_EXPENSE":
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
@@ -58,6 +84,11 @@ const filterReducerDefaultState = {
 };
 const filterReducer = (state = filterReducerDefaultState, action) => {
   switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return {
+        ...state,
+        text: action.text,
+      };
     default:
       return state;
   }
@@ -75,9 +106,16 @@ store.subscribe(() => {
 const expenseOne = store.dispatch(
   addExpense({ description: "tuition", amount: 400 })
 );
-store.dispatch(addExpense({ description: "why", amount: 800 }));
+const expenseTwo = store.dispatch(
+  addExpense({ description: "why", amount: 800 })
+);
 
-store.dispatch(removeExpense());
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+store.dispatch(setTextFilter("rent"));
+store.dispatch(setTextFilter());
+
 const demoState = {
   expenses: [
     {
@@ -95,3 +133,15 @@ const demoState = {
     endDate: undefined,
   },
 };
+
+const user = {
+  name: "Shaurya",
+  age: 18,
+};
+
+console.log({
+  // age: 17,//this wont have any effect since it is defined before spreading the object
+  ...user,
+  location: "JBL",
+  age: 17, //the age gets modified now..
+});
