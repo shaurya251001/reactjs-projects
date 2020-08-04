3887861b2860255d8347133080b1cad92dfb4c4c
shaurya251001 <shauryamarwah02@gmail.com>
2020-08-05 05:22:52 +0530

    wiring up add expense

--- expensify-app/src/components/AddExpense.js
+++ expensify-app/src/components/AddExpense.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
+import { connect } from "react-redux";
+import { addExpense } from "../actions/expenses";
 
-const AddExpensePage = () => {
+const AddExpensePage = (props) => {
   return (
     <div>
       Add expense page..
-      <ExpenseForm />
+      <ExpenseForm
+        onSubmit={(expense) => {
+          props.dispatch(addExpense(expense));
+          props.history.push("/");
+        }}
+      />
     </div>
   );
 };
-export default AddExpensePage;
+export default connect()(AddExpensePage);
