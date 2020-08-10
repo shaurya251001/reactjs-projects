2d938ce83ed14f0ac510d08ab5792cbbcf21d675
shaurya251001 <shauryamarwah02@gmail.com>
2020-08-10 18:01:24 +0530

    enzyme

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,10 +1,13 @@
 //react-test-renderer
-import ReactShallowRenderer from "react-test-renderer/shallow";
+
 import React from "react";
+import { shallow } from "enzyme";
 import Header from "../../components/Header";
 
 test("Should set header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
