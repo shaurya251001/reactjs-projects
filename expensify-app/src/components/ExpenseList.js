1f9e922c2a23252db5ba01953552a6bdda3fa055
shaurya251001 <shauryamarwah02@gmail.com>
2020-08-16 10:34:40 +0530

    snapshot testing with dynamic components

--- expensify-app/src/components/ExpenseList.js
+++ expensify-app/src/components/ExpenseList.js
@@ -3,13 +3,16 @@ import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
 import selectExpenses from "../selectors/expenses";
 
-const ExpenseList = (props) => {
+export const ExpenseList = (props) => {
   return (
     <div>
-      <h1>Expense List</h1>
-      {props.expenses.map((expense) => {
-        return <ExpenseListItem {...expense} key={expense.id} />;
-      })}
+      {props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+        props.expenses.map((expense) => {
+          return <ExpenseListItem {...expense} key={expense.id} />;
+        })
+      )}
     </div>
   );
 };
