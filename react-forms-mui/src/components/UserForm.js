ea4ece3b984ebd6597ccb5c28f0259f3255a256e
shaurya251001 <shauryamarwah02@gmail.com>
2020-10-02 10:26:30 +0530

    formuserdetails implemented

---
+++ react-forms-mui/src/components/UserForm.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import FormUserDetails from "./FormUserDetails";
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: "",
+  };
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1,
+    });
+  };
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1,
+    });
+  };
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+  render() {
+    const {
+      step,
+      firstName,
+      lastName,
+      email,
+      occupation,
+      city,
+      bio,
+    } = this.state;
+    const values = {
+      step,
+      firstName,
+      lastName,
+      email,
+      occupation,
+      city,
+      bio,
+    };
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return <h1>FormPersonalDetails</h1>;
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
+  }
+}
+
+export default UserForm;
