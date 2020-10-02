6b6aa5ff40a1f1a62060f16943433cfb41269b4e
shaurya251001 <shauryamarwah02@gmail.com>
2020-10-02 10:51:26 +0530

    confirm page implemented

--- react-forms-mui/src/components/UserForm.js
+++ react-forms-mui/src/components/UserForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FormPersonalDetails from "./FormPersonalDetails";
 import FormUserDetails from "./FormUserDetails";
+import Confirm from "./Confirm";
 
 export class UserForm extends Component {
   state = {
@@ -66,7 +67,14 @@ export class UserForm extends Component {
           />
         );
       case 3:
-        return <h1>Confirm</h1>;
+        return (
+          <Confirm
+            prevStep={this.prevStep}
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
       case 4:
         return <h1>Success</h1>;
     }
