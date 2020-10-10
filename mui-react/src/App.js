bf4cbea63a1eef18b5098d1bbb9bb2f3496236ac
shaurya251001 <shauryamarwah02@gmail.com>
2020-10-10 15:55:33 +0530

    button and buttonGroup

--- mui-react/src/App.js
+++ mui-react/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from "@material-ui/core/Button"
+import SaveIcon from "@material-ui/icons/Save"
+import DeleteIcon from "@material-ui/icons/Delete"
+import ButtonGroup from "@material-ui/core/ButtonGroup"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonGroup>
+        <Button startIcon={<SaveIcon/>} size="large" variant="contained" color="primary">
+          SAVE
+        </Button>
+        <Button startIcon={<DeleteIcon/>} size="large" variant="contained" color="secondary">
+          DISCARD
+        </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
