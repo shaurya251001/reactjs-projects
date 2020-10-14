d27163f8960e34c105259680d806f19879fa1609
shaurya251001 <shauryamarwah02@gmail.com>
2020-10-14 20:28:14 +0530

    checkboxes

--- mui-react/src/App.js
+++ mui-react/src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from "@material-ui/core/Button"
-import SaveIcon from "@material-ui/icons/Save"
-import DeleteIcon from "@material-ui/icons/Delete"
-import ButtonGroup from "@material-ui/core/ButtonGroup"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+function CheckBoxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <FormControlLabel
+      control={
+        <div>
+          <Checkbox
+            checked={checked}
+            icon={<SaveIcon/>}
+            checkedIcon={<DeleteIcon/>}
+            onChange={() => {
+              setChecked(!checked);
+            }}
+            color="primary"
+            
+          />
+        </div>
+      }
+      label="Testing a checkbox"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckBoxExample />
         <ButtonGroup>
-        <Button startIcon={<SaveIcon/>} size="large" variant="contained" color="primary">
-          SAVE
-        </Button>
-        <Button startIcon={<DeleteIcon/>} size="large" variant="contained" color="secondary">
-          DISCARD
-        </Button>
+          <Button
+            startIcon={<SaveIcon />}
+            size="large"
+            variant="contained"
+            color="primary"
+          >
+            SAVE
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            size="large"
+            variant="contained"
+            color="secondary"
+          >
+            DISCARD
+          </Button>
         </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        
       </header>
     </div>
   );
