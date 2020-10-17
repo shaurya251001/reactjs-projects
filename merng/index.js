23cfddceb8f39b850583cc69057ef06e19a343d0
shaurya251001 <shauryamarwah02@gmail.com>
2020-10-17 22:50:31 +0530

    database and server configured

--- merng/index.js
+++ merng/index.js
@@ -1,16 +1,30 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
+const { MONGODB } = require("./config");
+const Post = require("./models/Post");
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => {
-      return "Hello World!";
+    async getPosts() {
+      try {
+        const posts = await Post.find({});
+        return posts;
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
@@ -20,6 +34,9 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log("Connected to MongoDB");
+  return server.listen({ port: 5000 }).then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
 });
